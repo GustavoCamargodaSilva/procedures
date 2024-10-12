e3a5667fd8e80f999b548fcb63e7d075bd89fc5e
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2024-10-12 00:22:31 -0300

    initial project

---
+++ procedures/procedures/src/test/java/estudos/procedures/ProceduresApplicationTests.java
@@ -0,0 +1,13 @@
+package estudos.procedures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProceduresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
