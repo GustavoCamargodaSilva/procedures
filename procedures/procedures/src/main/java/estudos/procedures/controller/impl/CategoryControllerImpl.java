1daade2a8fbff74fea837396ff75a5b1f96ab31b
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2024-10-12 01:57:34 -0300

    alteracoes properties

--- procedures/procedures/src/main/java/estudos/procedures/controller/impl/CategoryControllerImpl.java
+++ procedures/procedures/src/main/java/estudos/procedures/controller/impl/CategoryControllerImpl.java
@@ -2,6 +2,8 @@ package estudos.procedures.controller.impl;
 
 import estudos.procedures.controller.CategoryController;
 import estudos.procedures.projection.CategoryProjection;
+import estudos.procedures.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -9,8 +11,11 @@ import java.util.List;
 @RestController
 public class CategoryControllerImpl implements CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @Override
     public List<CategoryProjection> getAllCategories() {
-        return List.of();
+        return categoryService.getAllCategories();
     }
 }
